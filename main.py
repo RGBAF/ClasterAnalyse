ed2f53032c95fd6d987ba8cff938a0370a70da9d
RGBAF <maxkire111@gmail.com>
2022-12-03 03:06:29 +0300

    Average Values

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ matrix = np.matrix([[120, 94, 164],
                     [145, 81, 120],
                     [78, 76.8, 86],
                     [70, 75.9, 104]])
+
+avg = lambda matrix_op: sum(matrix_op[0:]) / np.shape(matrix_op)[0]
+matrix = np.vstack([matrix, avg(matrix)])
+print(matrix)
